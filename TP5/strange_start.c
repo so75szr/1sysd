6dfc03bb9326d8e9cde620b92e3ba777d9176363
momperousse sorine <sosoobrezsoo@MacBook-Pro-SOSOOBREZSOO.local>
2024-01-28 19:42:41 +0100

    fin tp4

---
+++ TP5/strange_start.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// code des fonctions ici
+
+
+// code de test
+
+int main() {
+    char msg1[] = "Hello World!";
+    char m1[] = "MAJUSCULE";
+    char m2[] = "minuscule";
+    char m3[] = "MeLAnge";
+    char s1[] = "Une phrase simple.";
+    char s2[] = "Une autre phrase.";
+    char *s3;
+    char s4[] = "Debut et";
+    char s5[] = "Debut et suite";
+
+    printf("Longeur de msg1 (12 ?) %d\n", slen(msg1) );
+
+    // if (is_upper(m1)) {
+    //     printf("%s est en majuscules\n", m1)
+    // else {
+    //     printf("%s n'est pas en majuscule\n", m1)
+    // }
+
+    // if (is_upper(m2)) {
+    //     printf("%s est en majuscules\n", m2)
+    // else {
+    //     printf("%s n'est pas en majuscule\n", m2)
+    // }
+
+    // if (is_upper(m3)) {
+    //     printf("%s est en majuscules\n", m3)
+    // else {
+    //     printf("%s n'est pas en majuscule\n", m3)
+    // }
+
+    // printf("%s\n", s1)
+    // supper(s1);
+    // printf("s1 après passage en majuscule : %s\n", s1);
+    
+    // printf("%s\n", s2)
+    // slower(s2);
+    // printf("s2 après passage en minuscule : %s\n", s2);
+    
+    // s3 = scopy(s2);
+    // printf("Adresse de s2 : %lx\n", s2); 
+    // printf("Adresse de s3 : %lx\n", s3); 
+    // printf("s2 : %s\n", s2)
+    // printf("s3 : %s\n", s3)
+
+    // printf("s2 : %s s3 : %s\n", s2, s3);
+    // if ( sequal(s2, s3) ) {
+    //     printf("s2 et s3 ont le même contenu");
+    // } else {
+    //     printf("s2 et s3 ont un contenu différent");
+    // }
+    
+    // printf("s1 : %s s2 : %s\n", s1, s2);
+    // if ( sequal(s1, s2) ) {
+    //     printf("s1 et s2 ont le même contenu");
+    // } else {
+    //     printf("s2 et s2 ont un contenu différent");
+    // }
+    
+    // printf("s4 : %s s5 : %s\n", s4, s5);
+    // if ( sequal(s4, s5) ) {
+    //     printf("s4 et s5 ont le même contenu");
+    // } else {
+    //     printf("s4 et s5 ont un contenu différent");
+    // }
+    
+} 
+
